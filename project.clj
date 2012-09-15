7bf9435356ad517d85a5e51f370b7585d321b83b
Alyssa Kwan <alyssa.c.kwan@gmail.com>
2012-09-15 15:04:44 -0400

    ring init

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :main rudkus.core
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [ring "1.1.5"]
+                 [org.clojure/tools.cli "0.2.2"]])
--- src/rudkus/core.clj
+++ src/rudkus/core.clj
@@ -1,6 +1,32 @@
-(ns rudkus.core)
+(ns rudkus.core
+  (:require [ring.adapter.jetty :as jetty]
+            [clojure.tools.cli :as cli])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def handler
+  (fn [request]
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body "Hello, World!"}))
+
+(def ^{:private true} server (atom nil))
+
+(defn start [port]
+  (swap! server #(if (not (nil? %))
+                  (throw (IllegalStateException. "Server already started."))
+                  (jetty/run-jetty handler
+                                   {:port port
+                                    :join? false}))))
+
+(defn stop []
+  (swap! server #(if (nil? %)
+                  (throw (IllegalStateException. "Server already stopped."))
+                  (do (.stop %)
+                      nil))))
+
+(defn -main [& args]
+  (let [[options extra-args banner] (cli/cli args
+                                             ["-p" "--port" "Port" :default 80 :parse-fn #(Integer. %)])]
+    (if (not-empty extra-args)
+      (println banner)
+      (start (:port options)))))
